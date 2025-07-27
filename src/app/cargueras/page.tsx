69dfceb1899c9c6a41c11e875e8f2dd0c7c49335
Past Flex <past6867@gmail.com>
2025-07-27 23:06:54 +0000

    aun no se puede visualizar la lista en la seccion cargueras

--- src/app/cargueras/page.tsx
+++ src/app/cargueras/page.tsx
@@ -1,5 +1,14 @@
+import { getCargueras } from '@/services/cargueras';
+import { DataHydrator } from '@/components/layout/data-hydrator';
 import { CarguerasClient } from './cargueras-client';
 
-export default function CarguerasPage() {
-  return <CarguerasClient />;
+export default async function CarguerasPage() {
+  const cargueras = await getCargueras();
+
+  return (
+    <>
+      <DataHydrator cargueras={cargueras} />
+      <CarguerasClient />
+    </>
+  );
 }
