fd57213451f4d0a2f40481f73de22eda10409d27
Past Flex <past6867@gmail.com>
2025-08-04 08:26:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.map$/,
+      use: 'ignore-loader',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
